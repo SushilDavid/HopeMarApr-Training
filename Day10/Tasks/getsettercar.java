22759865ee65b729ef0577c7a39b9a2db810e296
SushilDavid <77227078+SushilDavid@users.noreply.github.com>
2026-04-06 09:52:59 +0530

    Add files via upload

---
+++ Day10/Tasks/getsettercar.java
@@ -0,0 +1,39 @@
+package Day10;
+
+
+
+public class getsettercar {
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.setBrand("Toyota");
+        c.setModel("Camry");
+        c.setYear(2022);
+        System.out.println("Car Brand: " + c.getBrand());
+        System.out.println("Car Model: " + c.getModel());
+        System.out.println("Car Year: " + c.getYear());
+    }
+}
+  
+class Car {
+    private String brand;
+    private String model;
+    private int year;
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public int getYear() {
+        return year;
+    }
+}
