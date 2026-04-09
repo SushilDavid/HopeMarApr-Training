a4433287cc44719b2c7665076c4dd901917b067b
SushilDavid <77227078+SushilDavid@users.noreply.github.com>
2026-04-09 10:47:52 +0530

    Add files via upload

---
+++ Majority.java
@@ -0,0 +1,23 @@
+package Day15;
+
+public class Majority {
+    
+class Solution {
+    public int majorityElement(int[] nums) {
+        int value=0;
+        int count=0;
+        for(int num:nums){
+            if(count==0){
+                value=num;
+            }
+            if(value==num){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return value;
+    }
+}
+}
