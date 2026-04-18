cc389309fc9f67ddd3360e3eeb0eb180a15bf46c
Sushil David <sushidavi006@gmail.com>
2026-04-18 08:58:57 +0530

    Fix condition in Stringsubstring class to correctly count non-repeating characters

--- Day20/Stringsubstring.java
+++ Day20/Stringsubstring.java
@@ -6,7 +6,7 @@ public class Stringsubstring {
         String s="xyzzaz";
         for(int i=0;i<s.length()-2;i++){
             
-            if(s.charAt(i)!=s.charAt(i+1) && s.charAt(i)!=s.charAt(i+2) ){
+            if(s.charAt(i)!=s.charAt(i+1) && s.charAt(i)!=s.charAt(i+2) && s.charAt(i+1)!=s.charAt(i+2)){
                count++;
             }
         }
